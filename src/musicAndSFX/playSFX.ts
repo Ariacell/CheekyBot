063164a014b13c42e9ebddd9dec7085c1061fa4a
Aria <aria.jackson@outlook.com.au>
2020-07-18 22:30:54 +1000

    Added youtube player library support

--- src/musicAndSFX/playSFX.ts
+++ src/musicAndSFX/playSFX.ts
@@ -1,14 +1,26 @@
 import * as Discord from 'discord.js';
+import ytdl from 'discord-ytdl-core';
 
 export const playSFX = (msg: any, args: string[]) => {
     // console.log(msg.member);
     const voxChannel: Discord.VoiceChannel = msg.member.voice.channel;
     voxChannel.join().then(connection => {
-        const dispatcher = connection.play(args[1]);
-        dispatcher.on("error", info => {
+
+        const stream = ytdl(args[1],
+            {
+                filter: 'audioonly',
+                opusEncoded: true,
+                encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+            });
+
+        console.log(JSON.stringify(stream));
+
+        const dispatcher = connection.play(stream, {type: "opus"})
+        .setVolume(0.25);
+        connection.on("error", info => {
             console.log(info);
         })
-        dispatcher.on("finish", end => {
+        .on("finish", end => {
             voxChannel.leave();
         })
     }).catch(err => {
