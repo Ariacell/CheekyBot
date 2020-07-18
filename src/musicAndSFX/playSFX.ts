2151d66db1881b2f7f9585d1d93e2f75b4d17571
Aria <aria.jackson@outlook.com.au>
2020-07-18 17:19:58 +1000

    Added basic url play functionality

--- src/musicAndSFX/playSFX.ts
+++ src/musicAndSFX/playSFX.ts
@@ -1,7 +1,19 @@
 import * as Discord from 'discord.js';
 
 export const playSFX = (msg: any, args: string[]) => {
-    const voxChannel = msg.member.voiceChannel;
+    // console.log(msg.member);
+    const voxChannel: Discord.VoiceChannel = msg.member.voice.channel;
+    voxChannel.join().then(connection => {
+        const dispatcher = connection.play(args[1]);
+        dispatcher.on("error", info => {
+            console.log(info);
+        })
+        dispatcher.on("finish", end => {
+            voxChannel.leave();
+        })
+    }).catch(err => {
+        console.log("Error joining voice channel: " + err);
+    })
 }
 
 export default playSFX;
